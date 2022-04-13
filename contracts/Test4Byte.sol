d77a3a08f0a7939e76cb698dfe3f75393f68e4d5
Daniel Luca <lucadanielcostin@gmail.com>
2022-04-13 11:53:09 +0000

    Testing webhook

---
+++ contracts/Test4Byte.sol
@@ -0,0 +1,39 @@
+contract WeirdContract {
+    event MsgSig(bytes4 sig);
+    event Bytes(bytes data);
+
+    error Aer__setParam_unrecognizedParam();
+    error Aer__unqueueDebt_auctionDelayNotPassed();
+    error Aer__settleDebtWithSurplus_insufficientSurplus();
+    error Aer__settleDebtWithSurplus_insufficientDebt();
+    error Aer__settleAuctionedDebt_notEnoughDebtOnAuction();
+    error Aer__settleAuctionedDebt_insufficientSurplus();
+    error Aer__startDebtAuction_insufficientDebt();
+    error Aer__startDebtAuction_surplusNotZero();
+    error Aer__startSurplusAuction_insufficientSurplus();
+    error Aer__startSurplusAuction_debtNotZero();
+    error Aer__transferCredit_insufficientCredit();
+    error Aer__lock_notLive();
+
+    function weirdName() public {
+        emit MsgSig(msg.sig);
+    }
+
+    function anotherWeirdName() public {
+        emit MsgSig(msg.sig);
+    }
+
+    function emitError() public {
+        revert Aer__setParam_unrecognizedParam();
+    }
+
+    function showError() public {
+        (bool success, bytes memory result) = address(this).call(
+            abi.encodeWithSelector(
+                this.emitError.selector
+            )
+        );
+
+        emit Bytes(result);
+    }
+}
